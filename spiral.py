a9ca99ddc955c45d56528e2af5bbfac5a88445b7
Muzaffer Kal <kal@dspia.com>
2022-08-12 22:32:01 -0700

    use np.zeros

--- spiral.py
+++ spiral.py
@@ -1,6 +1,6 @@
 import numpy as np
 N=3
-arr = [ [0]*N for _ in range(N)]
+arr = np.zeros((N, N), dtype=int)
 r = 1
 c = 1
 # direction stores 0=right, 1=down, 2=left, 3=up
@@ -11,39 +11,39 @@ def inc():
     match dir:
         case 0:
             if c < N and arr[r-1][c] == 0:
-                c = c + 1  
-            else: 
+                c = c + 1
+            else:
                 r = r + 1
                 dir = (dir + 1) % 4
         case 1:
             if r < N and arr[r][c-1] == 0:
-                r = r + 1  
-            else: 
+                r = r + 1
+            else:
                 c = c - 1
                 dir = (dir + 1) % 4
         case 2:
             if c > 1 and arr[r-1][c-2] == 0:
-                c = c - 1  
-            else: 
+                c = c - 1
+            else:
                 r = r - 1
                 dir = (dir + 1) % 4
         case 3:
             if r > 1 and arr[r-2][c-1] == 0:
-                r = r - 1  
-            else: 
-                c = c + 1  
+                r = r - 1
+            else:
+                c = c + 1
                 dir = (dir + 1) % 4
 #print(arr)
 
-for r in range(N):
-    for c in range(N):
-        arr[r][c] = r*N+c
+#for r in range(N):
+#    for c in range(N):
+#        arr[r][c] = r*N+c
+#
+#for r in range(N):
+#    for c in range(N):
+#        arr[r][c] = 0
 
-for r in range(N):
-    for c in range(N):
-        arr[r][c] = 0
-
-print(np.matrix(arr))
+print(arr)
 r = 1
 c = 1
 
@@ -53,4 +53,4 @@ for x in range(1, N*N+1):
     #print(arr)
     inc()
 
-print(np.matrix(arr))
+print(arr)
